a9c279b9a717828047acb563e217fb4834735a14
NefytZ <thyphen.merigot@gmail.com>
2022-10-31 20:45:02 +0100

    challenge4

--- Car.php
+++ Car.php
@@ -8,15 +8,15 @@ class Car extends Vehicle
         'fuel',
         'electric',
     ];
-
+    private bool $hasParkBrake = false;
     private string $energy;
 
     private int $energyLevel;
 
     public function __construct(string $color, int $nbSeats, string $energy)
     {
- parent::__construct($color, $nbSeats);
-    $this->setEnergy($energy);
+        parent::__construct($color, $nbSeats);
+        $this->setEnergy($energy);
     }
 
 
@@ -42,5 +42,33 @@ class Car extends Vehicle
     public function setEnergyLevel(int $energyLevel): void
     {
         $this->energyLevel = $energyLevel;
+    }
+
+    public function setHasParkBrake()
+    {
+        if ($this->hasParkBrake == true) {
+            $this->hasParkBrake = false;
+            return $this->start();
+        } elseif ($this->hasParkBrake == false) {
+            $this->hasParkBrake = true;
+            return 'Mise du frein à main.';
+        }
+        return $this;
+    }
+
+    public function getHasParkBrake(): bool
+    {
+        return $this->hasParkBrake;
+    }
+
+
+
+    public function start(): string
+    {
+        if ($this->hasParkBrake === true) {
+            throw new Exception('Impossible d\'avancer. Veuillez débloquer le frein à main.');
+        } else {
+            return "Moteur en marche, frein à main enlever, prêt à partir: Let's Go!";
+        }
     }
 }
--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 require_once 'Bicycle.php';
-require_once 'Cars.php';
+require_once 'Car.php';
 require 'Truck.php';
 require 'bus.php';
 
 $renault = new Truck('green', 5, 100, 100,);
-var_dump ($renault->setLoading(99));
-var_dump ($renault->setLoading(200));
+var_dump($renault->setLoading(99));
+var_dump($renault->setLoading(200));
 echo 'The Renault ' . $renault->forward();
 echo $renault->brake();
 
 $velib = new Bicycle('green', 1);
-$bmw = new Car('black', 5, 'gazoil' );
-$aygo = new Car ('white', 4, 'fuel' );
-$bmx = new Bicycle ('white', 1);
+$bmw = new Car('black', 5, 'gazoil');
+$aygo = new Car('white', 4, 'fuel');
+$bmx = new Bicycle('white', 1);
 
 echo 'The Bmw ' . $bmw->forward();
 echo $bmw->brake();
@@ -31,6 +31,21 @@ echo $bmx->brake();
 $tesla = new Car('black', 4, 'electric');
 $ferrari = new Car('red', 2, 'fuel');
 $schoolBus = new Bus('yellow', 5, 100, 100,);
-VAR_DUMP ($tesla);
-VAR_DUMP ($ferrari);
+VAR_DUMP($tesla);
+VAR_DUMP($ferrari);
 
+
+
+
+
+// Actionnement du frein à main au démarrage
+echo $ferrari->setHasParkBrake();
+
+try {
+    echo $ferrari->start() . PHP_EOL;
+} catch (Exception $exception) {
+    echo $exception->getMessage() . PHP_EOL;
+    echo $ferrari->setHasParkBrake() . PHP_EOL;
+} finally {
+    echo "Ma voiture roule comme un donut";
+}
