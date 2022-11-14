7eb2cf698a206d36a6de1b147e03bbaa0cadd64c
NefytZ <thyphen.merigot@gmail.com>
2022-11-14 10:00:21 +0100

    POO - Basics | Part 6 : Static

--- speedometer.php
+++ speedometer.php
@@ -1,22 +1,18 @@
 <?php
 
 
-
 class Speedometer
 {
 
     const MILES = 0.6212;
     const KM = 1.609;
 
-    public static function convertMilesToKm($number)
+    public static function convertMilesToKm(int $number)
     {
-
-        return $number = $number * self::KM;
-
-
+        return $number = $number / self::KM;
     }
 
-    public static function convertKmToMiles($number)
+    public static function convertKmToMiles(int $number)
     {
         return $number = $number * self::MILES;
     }
